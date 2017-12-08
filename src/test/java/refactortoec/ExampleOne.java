552a213af364c246327b1707da882d84d5bbfad2
vmzakharov <zakharov.vladimir.m@gmail.com>
2017-12-07 22:35:46 -0500

    initial setup of the project structure

---
+++ src/test/java/refactortoec/ExampleOne.java
@@ -0,0 +1,13 @@
+package refactortoec;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleOne
+{
+    @Test
+    public void allIsWell()
+    {
+        Assert.assertTrue(true);
+    }
+}
